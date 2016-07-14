2ede0ff6cf74f0da5c5c3e94b6d480cd5cae3f79
Carina Boo <carinaboo@Carinas-MacBook-Pro.local>
2016-07-13 21:24:25 -0700

    Basic single-screen tip calculator with bill field, tip, total, and 3 tip percentage control.

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercentageControl: UISegmentedControl!
+    
+    let tipPercentages = [0.18, 0.20, 0.25]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipPercentageControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
